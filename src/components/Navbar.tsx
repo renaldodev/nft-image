f35b0a4ad31b94d1d256b3a107a6c6a68c07ed9c
renaldodev <renaldomateus77.dev@gmail.com>
2022-03-15 00:17:29 +0100

    refactor(components/Navbar): add fixed position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,12 @@ const WapperContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.colors.black};
+  z-index: 999;
 `;
 const RigthContainer = styled.div`
   display: flex;
@@ -17,15 +23,15 @@ const LeftContainer = styled.div`
   gap: 1rem;
 `;
 const Logo = styled.h1`
-   font-size: 1.5rem;
+  font-size: 1.5rem;
 `;
 const SearchContainer = styled.div`
   display: flex;
-  width:20rem;
+  width: 20rem;
   height: 2rem;
   gap: 1rem;
   align-items: center;
-  background-color:${props => props.theme.colors.gray};
+  background-color: ${(props) => props.theme.colors.gray};
   padding: 0.5rem;
   border-radius: 0.5rem;
 `;
@@ -34,30 +40,30 @@ const Input = styled.input`
   background: transparent;
   border: none;
   outline: none;
-  color: ${props => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.white};
 `;
 const Nav = styled.nav`
   display: flex;
   gap: 1rem;
 `;
 const NavItem = styled.p`
-   font-size: 1rem;
-   cursor: pointer;
+  font-size: 1rem;
+  cursor: pointer;
 `;
 const ButtonWalletContainer = styled.button`
-    display: flex;
-    background-color: ${props => props.theme.colors.pink};
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    border-radius: 0.5rem;
-    border: 0;
-    padding: 0.5rem;
-    gap: 0.5rem;
-    color:white;
+  display: flex;
+  background-color: ${(props) => props.theme.colors.pink};
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  border: 0;
+  padding: 0.5rem;
+  gap: 0.5rem;
+  color: white;
 `;
 const ButtonWalletText = styled.span`
-   color: ${props => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.white};
 `;
 
 export default function Navbar() {
